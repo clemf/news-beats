9e02b3b53c3f0db3fc229a67cacf6717807e1091
Clem <clemfree@gmail.com>
2015-03-11 15:18:26 -0700

    Clear form input

--- controllers/BeatsController.js
+++ controllers/BeatsController.js
@@ -2,5 +2,6 @@ newsBeats.controller('BeatsCtrl', function (BeatsFactory) {
   this.allBeats = BeatsFactory.allBeats;
   this.addBeat = function() {
     BeatsFactory.addBeat(this.beatName);
+    this.beatName = null;
   };
 });
